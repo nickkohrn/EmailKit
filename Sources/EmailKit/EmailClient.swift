750a9ac66f00b401736189fc63417cc190030855
Nick Kohrn <bnkohrn@icloud.com>
2023-06-19 09:39:28 -0400

    Add action handlers

--- Sources/EmailKit/EmailClient.swift
+++ Sources/EmailKit/EmailClient.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 public struct EmailClient {
+    public var canSendMail: @MainActor @Sendable () async -> Bool
     public var openEmail: @MainActor @Sendable (EmailConfiguration) async -> Void
 }
