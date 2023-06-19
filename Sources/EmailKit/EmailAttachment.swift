328b024a66f07ab8862d9e1e4236518e43a15a4f
Nick Kohrn <bnkohrn@icloud.com>
2023-06-19 00:08:56 -0400

    Make inits public

--- Sources/EmailKit/EmailAttachment.swift
+++ Sources/EmailKit/EmailAttachment.swift
@@ -3,4 +3,12 @@ import Foundation
 public struct EmailAttachment {
     public let data: Data
     public let filename: String
+
+    public init(
+        data: Data,
+        filename: String
+    ) {
+        self.data = data
+        self.filename = filename
+    }
 }
